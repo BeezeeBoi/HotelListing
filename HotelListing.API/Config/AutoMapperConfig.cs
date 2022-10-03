7931b9d838de461f1ab20ca23393512f706ce1dd
Beezee_Boi <znoobtube@yahoo.com>
2022-10-03 19:24:37 -0400

    add auto mapper config file

---
+++ HotelListing.API/Config/AutoMapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HotelListing.API.Data;
+using HotelListing.API.Models.Country;
+
+namespace HotelListing.API.Config
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Country, CreateCountryDO>().ReverseMap();
+        }
+    }
+}
