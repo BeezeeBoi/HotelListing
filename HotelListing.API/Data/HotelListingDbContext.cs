ea88fded98aa8076d143f22718b69e2997aced8e
Beezee_Boi <beezeeboi.znoobtube@gmail.com>
2022-10-02 22:55:37 -0400

    seed database

--- HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -11,5 +11,41 @@ namespace HotelListing.API.Data
 
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+                new Country
+                {
+                    Id = 2, 
+                    Name = "United States of America", 
+                    ShortName = "USA"
+                });
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Holiday Inn",
+                    Address = "George Town",
+                    CountryId = 1,
+                    Rating = 3.8
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Hilton",
+                    Address = "Knoxville",
+                    CountryId = 2,
+                    Rating = 4.5
+                });
+        }
     }
 }
