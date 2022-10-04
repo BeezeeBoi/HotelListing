634f1cdb6da96f96b80727db26304eade9c008c7
Beezee_Boi <znoobtube@yahoo.com>
2022-10-04 17:22:45 -0400

    add region migration and seed database

---
+++ HotelListing.API/Migrations/20221004212110_RegionPropertyAdded.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Migrations
+{
+    public partial class RegionPropertyAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Countries");
+
+            migrationBuilder.CreateTable(
+                name: "Region",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CountryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Region", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Region_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Region",
+                columns: new[] { "Id", "CountryId", "Name" },
+                values: new object[] { 1, 1, "Central America" });
+
+            migrationBuilder.InsertData(
+                table: "Region",
+                columns: new[] { "Id", "CountryId", "Name" },
+                values: new object[] { 2, 2, "North America" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Region_CountryId",
+                table: "Region",
+                column: "CountryId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Region");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Region",
+                value: "Central America");
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Region",
+                value: "North America");
+        }
+    }
+}
