f94cb0007b7b3301a273045730012f10a4c160c6
Beezee_Boi <znoobtube@yahoo.com>
2022-10-04 00:14:49 -0400

    add get country data object

---
+++ HotelListing.API/Models/Country/GetCountryDO.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelListing.API.Models.Country
+{
+    public class GetCountryDO
+    {
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string Region { get; set; }
+    }
+}
