cdfe238743d6cfab9572da1209b5c27a1ae666a0
Beezee_Boi <znoobtube@yahoo.com>
2022-10-06 22:16:35 -0400

    query for address and return in single query request

--- HotelListing.API/Repository/HotelsRepository.cs
+++ HotelListing.API/Repository/HotelsRepository.cs
@@ -1,12 +1,25 @@
 ﻿using HotelListing.API.Contracts;
 using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.API.Repository;
 
 public class HotelsRepository : GenericRepository<Hotel>, IHotelsRepository
 {
+    readonly HotelListingDbContext _context;
+
     public HotelsRepository(HotelListingDbContext context) : base(context)
     {
+        _context = context;
+    }
 
+    public new async Task<Hotel> GetAsync(int? id)
+    {
+        if (id == null)
+        {
+            return null;
+        }
+
+        return await _context.Hotels.Include(q => q.Address).FirstOrDefaultAsync(q => q.Id == id);
     }
 }
