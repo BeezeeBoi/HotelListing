10b464911f591ea3a1fa98d4ead3a54155e5898d
Beezee_Boi <beezeeboi.znoobtube@gmail.com>
2022-10-07 02:57:11 -0400

    remove get method in hotel repository

--- HotelListing.API/Repository/HotelsRepository.cs
+++ HotelListing.API/Repository/HotelsRepository.cs
@@ -1,25 +1,12 @@
 ﻿using HotelListing.API.Contracts;
 using HotelListing.API.Data;
-using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.API.Repository;
 
 public class HotelsRepository : GenericRepository<Hotel>, IHotelsRepository
 {
-    readonly HotelListingDbContext _context;
-
     public HotelsRepository(HotelListingDbContext context) : base(context)
     {
-        _context = context;
-    }
-
-    public new async Task<Hotel> GetAsync(int? id)
-    {
-        if (id == null)
-        {
-            return null;
-        }
-    
-        return await _context.Hotels.Include(t => t.Address).FirstOrDefaultAsync(q => q.Id == id);
+        
     }
 }
